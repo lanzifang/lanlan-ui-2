18c2e3181bb69580b1916e4ce0bea2c5d873de74
Nancy <1033580067@qq.com>
2023-10-11 17:48:11 +0800

    更改

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Fang />
+</template>
+
+<script>
+import Fang from './components/Fang.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Fang
+  }
+}
+</script>
