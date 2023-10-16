41322e7ff51a6a6df1fc3b10915f90acfcbcf090
Nancy <1033580067@qq.com>
2023-10-16 11:50:03 +0800

    初始化Doc页面aside的html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
             <h1>蓝蓝UI</h1>
             <h1>一个简单且实用的UI框架</h1>
             <p class="actions">
-                <a>GitHub</a>
-                <a>开始</a>
+                <a href="https://github.com">GitHub</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
